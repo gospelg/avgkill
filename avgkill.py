1e44281a1b435da5d30502b2e9ca050e6a5210eb
gospelg <garrettcrosby@outlook.com>
2017-07-08 23:13:55 -0400

    avgkill 1.0
    create avgkill

---
+++ avgkill.py
@@ -0,0 +1,40 @@
+from time import sleep
+import subprocess
+import logging
+import os
+
+def avgkill():
+    logging.info('starting avgmfapx.exe')
+    subprocess.call('"C:\\Program Files (x86)\\AVG\\AV\\avgmfapx.exe"'
+                     ' /Appmode=Setup /uninstall /uilevel=Silent', shell=True)
+
+def still_run(process):
+    running = subprocess.check_output('tasklist', shell=True)
+    if process in running:
+        sleep(5)
+        logging.info('avgmfapx.exe is still running...')
+        still_run(process)
+    else:
+        logging.info('avgmfapx.exe is complete, moving to cleanup.')
+        cleanup()
+        
+
+def cleanup():
+    try:
+        subprocess.call('rd /s /q "C:\\programdata\\AVG"', shell=True)
+        subprocess.call('rd /s /q "C:\\program files (x86)\\AVG"', shell=True)
+        subprocess.call('rd /s /q "C:\\users\\%USERNAME%\\appdata\\local\\AVG"', shell=True)
+        subprocess.call('rd /s /q "C:\\users\\%USERNAME%\\appdata\\roaming\\AVG"', shell=True)
+        logging.info('cleanup successful.')
+    except:
+        logging.error('cleanup failed.')
+
+def main():
+    if not os.path.exists('C:\\avgkill'):
+        os.makedirs('C:\\avgkill')
+    logging.basicConfig(filename='C:\\avgkill\\avgkill.log',
+                        level=logging.DEBUG)
+    avgkill()
+    still_run('avgsetupx.exe')
+
+main()
